dc39b42f39f038f3480111d5fb39a84f68dca4fc
Albert Lai <albertlai631@outlook.com>
2025-05-08 08:13:51 +0900

    set configs

--- config/development.ts
+++ config/development.ts
@@ -9,17 +9,14 @@ export default {
       password: '',
       type: '',
     },
-
     uriPrefix: 'mongodb://',
-    uri: 'eco-mongo-db:27017',
-    // uri: 'localhost:27017',
+    uri: 'localhost:27017',
     dbName: 'eco-solver-local',
     enableJournaling: true,
   },
   redis: {
     connection: {
-      host: 'eco-redis',
-      // host: 'localhost',
+      host: 'localhost',
       port: 6379,
     },
     jobs: {
@@ -37,8 +34,4 @@ export default {
       metalayer_duration_seconds: 180,
     },
   },
-  // kms: {
-  //   region: 'us-west-2',
-  //   keyID: '0a74ee2e-cbd8-43d2-8ff9-85f39b581cf7',
-  // },
 }
