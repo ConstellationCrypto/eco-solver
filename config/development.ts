646b1531a5b16a6813b7f6a62454f461d7ee4c6d
Albert Lai <albertlai631@outlook.com>
2025-05-08 10:12:48 +0900

    move intentconfigs to aws secrets

--- config/development.ts
+++ config/development.ts
@@ -27,11 +27,4 @@ export default {
       },
     },
   },
-  intentConfigs: {
-    proofs: {
-      storage_duration_seconds: 60,
-      hyperlane_duration_seconds: 120,
-      metalayer_duration_seconds: 180,
-    },
-  },
 }
