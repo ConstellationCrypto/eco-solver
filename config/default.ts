00d2421c2d6ea35703679e75d98fbbb401b81ab3
Chenyao Yu <4844716+chenyaoy@users.noreply.github.com>
2025-05-26 19:09:45 -0400

    Initial changes

--- config/default.ts
+++ config/default.ts
@@ -1,16 +1,16 @@
 export default {
   aws: [
     {
-      region: 'us-east-2',
-      secretID: 'eco-solver-secrets-dev',
+      region: 'us-west-2',
+      secretID: 'eco-solver-secrets-prod-test',
     },
     {
-      region: 'us-east-2',
-      secretID: 'eco-solver-configs-dev',
+      region: 'us-west-2',
+      secretID: 'eco-solver-configs-prod-test',
     },
     {
-      region: 'us-east-2',
-      secretID: 'eco-solver-whitelist-dev',
+      region: 'us-west-2',
+      secretID: 'eco-solver-whitelist-prod-test',
     },
   ],
   cache: {
@@ -185,6 +185,9 @@ export default {
   hyperlane: {
     useHyperlaneDefaultHook: false,
   },
+
+  rpcs: {},
+  warpRoutes: {},
 
   externalAPIs: {},
   logger: {
